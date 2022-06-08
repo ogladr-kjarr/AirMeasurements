f6651cdb7d293d6c6f3af2afbbb355be60cece48
óglaðr kjarr <40721677+ogladr-kjarr@users.noreply.github.com>
2022-06-08 18:40:11 +0100

    Added postgres access

---
+++ src/main/resources/PostgreSQLSchema.sql
@@ -0,0 +1,13 @@
+createdb measurements;
+
+create table measurements(
+mdate date,
+location varchar,
+parameter varchar,
+interval varchar,
+unit varchar,
+value double precision,
+status varchar);
+
+docker run -p 5432:5432 -e POSTGRES_PASSWORD=testingjava postgres:latest
+docker exec -it container_name bash
