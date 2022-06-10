eafa2334f9023a91ccc7b8b8649ac75f372b30b1
ogladr-kjarr <40721677+ogladr-kjarr@users.noreply.github.com>
2022-06-10 13:20:26 +0100

    removed redundance imports

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,7 @@
-import model.CSVDownloader;
-import model.CSVLoader;
-import model.Measurement;
-import model.PostgreSQLAccess;
+import model.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
