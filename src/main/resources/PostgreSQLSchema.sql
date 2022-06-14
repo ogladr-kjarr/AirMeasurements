3e3faab850c523bce359f67ee4105f30e85be1fc
ogladr-kjarr <40721677+ogladr-kjarr@users.noreply.github.com>
2022-06-14 13:17:38 +0100

    Docker compose now creates PG instance and DB

--- src/main/resources/PostgreSQLSchema.sql
+++ src/main/resources/PostgreSQLSchema.sql
@@ -1,14 +1,10 @@
-createdb measurements;
-
 create table measurements(
-mdate timestamp with time zone,
-location varchar,
-parameter varchar,
-interval varchar,
-unit varchar,
-value double precision,
-status varchar,
-PRIMARY KEY (mdate, location, parameter));
-
-docker run --name measurement-db -p 5432:5432 -e POSTGRES_PASSWORD=testingjava postgres:latest
-docker exec -it measurement-db bash
+    mdate timestamp with time zone,
+    location varchar,
+    parameter varchar,
+    interval varchar,
+    unit varchar,
+    value double precision,
+    status varchar,
+    PRIMARY KEY (mdate, location, parameter)
+    );
